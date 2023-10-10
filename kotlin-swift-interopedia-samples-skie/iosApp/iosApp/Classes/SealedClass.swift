0f1c2bffae4ded467226b3a6d773cae38e51bb68
pahill <pamela.a.hill@gmail.com>
2023-10-10 18:06:16 +0200

    Adding SKIE, restarting

--- kotlin-swift-interopedia-samples-skie/iosApp/iosApp/Classes/SealedClass.swift
+++ kotlin-swift-interopedia-samples-skie/iosApp/iosApp/Classes/SealedClass.swift
@@ -5,8 +5,8 @@ class SealedClassExample {
     func example(s: SealedClass) {
         switch onEnum(of: s) {
             case .object: print("object")
-        case .simple(let simple): print("simple \(simple.param1)")
-        case .data(let data): print("data \(data.param1) \(data.param2)")
+            case .simple(let simple): print("simple \(simple.param1)")
+            case .data(let data): print("data \(data.param1) \(data.param2)")
         }
     }
 }
